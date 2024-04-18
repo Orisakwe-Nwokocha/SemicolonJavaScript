063f7e85423534831e908de8d920387f4c569c15
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-04-18 10:12:59 +0100

    new commit

---
+++ april_18_2024.js
@@ -0,0 +1,12 @@
+function findTheHcfOf(numbers) {
+    let max = 0;
+    for (let number of numbers) if (number > max) max = number;
+    for (let hcf = max; hcf >= 1; hcf--) {
+        let count = 0;
+        for (let number of numbers) if (number % hcf === 0) count++;
+        if (count === numbers.length) return hcf;
+    }
+}
+
+console.log(findTheHcfOf([4, 8, 8, 16]))
+console.log(findTheHcfOf([9, 9, 12]))
