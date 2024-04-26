83471e69694497a6f4700cbb2fa3d37ea748fb53
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-04-26 22:02:44 +0100

    added new algorithm

--- FreeCodeCamp/FCC.js
+++ FreeCodeCamp/FCC.js
@@ -70,19 +70,19 @@ console.log(spinalCase('This Is Spinal Tap'));
 function translatePigLatin(str) {
     str = str.toLowerCase();
     let vowels = ["a", "e", "i", "o", "u"];
-    if (vowels.includes(str[0])) return str.slice(0, 1) + str.slice(1) + "way";
+    if (vowels.includes(str[0])) return str + "way";
 
     let split = str
         .replace(/([a-z])/g, "$1 ")
         .trim()
         .split(" ");
 
-    function check(split) {
-        for (let i = 0; i < split.length; i++) if (vowels.includes(split[i])) return i;
+    function findFirstVowelIndex(split) {
+        for (let index = 0; index < split.length; index++) if (vowels.includes(split[index])) return index;
         return -1;
     }
 
-    let index = check(split);
+    let index = findFirstVowelIndex(split);
     if (index === -1) return str + "ay";
     return str.slice(index) + str.slice(0, index) + "ay";
 }
@@ -101,6 +101,9 @@ console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "l
 console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms"));
 console.log(myReplace("I think we should look up there", "up", "Down"));
 
+
+/**
+ * First try
 
 function pairElement(str) {
     let split = str.replace(/([A-Z])/g, "$1 ").trim().split(" ");
@@ -122,6 +125,53 @@ function pairElement(str) {
         }
     })
     return output;
+}*/
+
+// second try
+function pairElement(str) {
+    const chars = str.split("");
+    const basePairs = {
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C"
+    };
+
+    return chars.map(char => [char, basePairs[char]]);
 }
 
 console.log(pairElement("GCG"));
+
+
+/**
+ *  first try
+
+function fearNotLetter(str) {
+    let alphabets = [
+        'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z'
+    ];
+
+    const getIndex = alphabets.findIndex(char => char === str[0]);
+    let counter = 0;
+    for (let index = getIndex; index < alphabets.length; index++) {
+        if (alphabets[index] !== str[counter++]) return alphabets[index];
+        if (counter === str.length) break;
+    }
+}*/
+
+// second try
+function fearNotLetter(str) {
+    let alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+    const startIndex = alphabets.indexOf(str[0]);
+    for (let index = 0; index < str.length; index++)
+        if (str[index] !== alphabets[startIndex + index]) return alphabets[startIndex + index];
+}
+
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("stvwx"));
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
