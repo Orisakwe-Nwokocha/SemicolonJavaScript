9b4cddd811853c9f0a904d746bd32adfe06c6d0f
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-05-01 09:42:48 +0100

    not to brag, but I'm a genius - maybe

--- FreeCodeCamp/FCC.js
+++ FreeCodeCamp/FCC.js
@@ -311,3 +311,68 @@ function dropElements(arr, func) {
 console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
 console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
 console.log(dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}))
+
+
+function steamrollArray(arr) {
+    let output = [];
+    for (const element of arr) {
+        if (Array.isArray(element)) {
+            let subArray = steamrollArray(element);
+            output = output.concat(subArray);
+            continue;
+        }
+        output.push(element);
+    }
+    return output;
+}
+
+console.log(steamrollArray([1, [2], {}, [3, [[4]]]]));
+
+
+function binaryAgent(str) {
+    function toDecimalFrom(binary) {
+        let decimal = 0;
+        let power = 0;
+        for (let index = binary.length - 1; index >=0; index--) {
+            let digit = parseInt(binary[index]);
+            decimal += digit * Math.pow(2, power);
+            power++;
+        }
+        return decimal;
+    }
+
+
+    return str.split(" ")
+        .map(binary => {
+            let codePoint = toDecimalFrom(binary);
+            return String.fromCodePoint(codePoint);
+        })
+        .join("");
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+console.log(binaryAgent("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001"));
+
+
+function truthCheck(collection, pre) {
+    let counter = 0;
+    collection.forEach((item) => {
+        for (const key in item) if (key === pre && Boolean(item[key])) counter++;
+    });
+
+    return collection.length === counter;
+}
+
+
+console.log(truthCheck([
+    {name: "Quincy", role: "Founder", isBot: false},
+    {name: "Naomi", role: "", isBot: false},
+    {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+console.log(truthCheck([
+    {name: "Pikachu", number: 25, caught: 3},
+    {name: "Togepi", number: 175, caught: 1},
+    {name: "MissingNo", number: NaN, caught: 0}], "number"));
+console.log(truthCheck([
+    {name: "Pikachu", number: 25, caught: 3},
+    {name: "Togepi", number: 175, caught: 1}], "number"));
+
